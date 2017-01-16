067cad33ccadb116d9e362924deadb82780f4780
thomaslaisnez <thomaslaisnez@live.be>
2017-01-16 12:59:05 +0100

    First push
    beerApp

---
+++ beerApplication/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  beerApplication
+//
+//  Created by Thomas Laisnez on 13/01/17.
+//  Copyright © 2017 Thomas Laisnez. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
---
+++ beerApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  beerApplication
+//
+//  Created by Thomas Laisnez on 12/01/17.
+//  Copyright © 2017 Thomas Laisnez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func cancelToPlayersViewController(segue:UIStoryboardSegue) {
+    }
+
+}
+
